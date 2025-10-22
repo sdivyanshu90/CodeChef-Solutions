0db527b562068205f674a204e0a5b9e02da142f0
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-22 20:24:30 +0530

    Chef Bakes Cake 1

---
+++ zero_to_1000/bakecake1.py
@@ -0,0 +1,7 @@
+# Question Link: https://www.codechef.com/problems/BAKECAKE1
+
+# cook your dish here
+n, m = map(int, input().split())
+cost_price = n * 30
+selling_price = m * 50
+print(selling_price - cost_price)
