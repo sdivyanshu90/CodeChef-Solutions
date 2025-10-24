1806c4dad81ccfadab43333c8b7dbed925ce4d10
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-24 23:39:15 +0530

    Make Cat

--- zero_to_1000/incat.py
+++ zero_to_1000/incat.py
@@ -16,3 +16,18 @@ if c_count == 1 and a_count == 1 and t_count == 1:
     print("Yes")
 else:
     print("No")
+
+
+# Approach 2: Using Stack
+# cook your dish here
+s = input().strip()
+stack = ["c", "a", "t"]
+
+for char in s:
+    if char in stack:
+        stack.remove(char)
+        
+if len(stack) == 0:
+    print("Yes")
+else:
+    print("No")
