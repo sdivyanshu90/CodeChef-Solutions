c607854f38f130b099ba9eb0e727601177913f82
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-26 22:29:39 +0530

    Advitiya

---
+++ zero_to_1000/advitiya1.py
@@ -0,0 +1,8 @@
+# Question Link: https://www.codechef.com/problems/ADVITIYA1
+
+# cook your dish here
+n = int(input())
+if 16 <= n <= 18:
+    print("ADVITIYA")
+else:
+    print("WAITING FOR ADVITIYA")
