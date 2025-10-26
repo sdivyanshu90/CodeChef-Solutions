56edfdaee960fa99a327ae5273deee28f4738d38
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-26 21:34:36 +0530

    Double Rent

---
+++ zero_to_1000/doublerent.py
@@ -0,0 +1,16 @@
+# Question Link: https://www.codechef.com/problems/DOUBLERENT
+
+# cook your dish here
+x = int(input())
+print(2 * x)
+
+# Approach 2: Using bitwise left shift
+# x = int(input())
+# print(x << 1)  # Left shift by 1 is equivalent to multiplying by 2
+
+# Approach 3: Using addition
+# x = int(input())
+# print(x + x)  # Adding the number to itself to double it
+
+# Approach 4: More Pythonic
+# print(int(input()) * 2)  # Directly reading input and printing doubled value
