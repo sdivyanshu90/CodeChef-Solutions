1506b7dc95b97a246ce7418967c08e4c986aef4e
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-26 21:40:55 +0530

    Lucky Clover

---
+++ zero_to_1000/luclo.py
@@ -0,0 +1,10 @@
+# Question Link: https://www.codechef.com/problems/LUCLO
+
+# cook your dish here
+n = int(input())
+rem = n - 1
+print(4 + rem * 3)
+
+# Approach 2: Using formula directly
+# n = int(input())
+# print(3 * n + 1)  # Direct formula to calculate the result
