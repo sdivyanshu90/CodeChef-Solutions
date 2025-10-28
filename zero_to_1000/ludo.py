a72fed84070cb7b86894c59ecf217ceea3cfde9e
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-28 22:42:39 +0530

    Chef Plays Ludo

---
+++ zero_to_1000/ludo.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/LUDO
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x == 6:
+        print("YES")
+    else:
+        print("NO")
