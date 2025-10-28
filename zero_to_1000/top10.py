df5ec8fb8b68c40dabc7d2813b18ba705366d409
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-28 20:35:47 +0530

    Masterchef finals

---
+++ zero_to_1000/top10.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/TOP10
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x <= 10:
+        print("YES")
+    else:
+        print("NO")
