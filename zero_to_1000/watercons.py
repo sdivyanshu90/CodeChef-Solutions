1e3f614a930cd7a6dc9c5a3655ec112fcff2b4e2
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-28 20:34:13 +0530

    Water Consumption

---
+++ zero_to_1000/watercons.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/WATERCONS
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x >= 2000:
+        print("YES")
+    else:
+        print("NO")
