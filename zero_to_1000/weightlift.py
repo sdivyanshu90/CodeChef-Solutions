7a497d7e7031561542f61af991408fa2c91b7d28
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-28 23:13:39 +0530

    Weightlifting

---
+++ zero_to_1000/weightlift.py
@@ -0,0 +1,21 @@
+# Question Link: https://www.codechef.com/problems/WEIGHTLIFT
+
+# cook your dish here
+a, b, c, d, e, f = map(int, input().split())
+res = 0
+if a > b:
+    res += a
+else:
+    res += b
+    
+if c > d:
+    res += c
+else:
+    res += d
+    
+if e > f:
+    res += e
+else:
+    res += f
+    
+print(res)
