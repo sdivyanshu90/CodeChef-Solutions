9375300d21062ba0428c2800ff47d99599e6a505
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-31 00:09:08 +0530

    Who is taller!

---
+++ zero_to_1000/taller.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/TALLER
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if a > b:
+        print("A")
+    else:
+        print("B")
