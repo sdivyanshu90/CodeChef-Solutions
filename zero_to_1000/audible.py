5c13d8d6ce8abc1a230cd2b391b480bd5a480e62
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-30 23:45:11 +0530

    Audible Range

---
+++ zero_to_1000/audible.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/AUDIBLE
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if 67 <= x <= 45000:
+        print("YES")
+    else:
+        print("NO")
