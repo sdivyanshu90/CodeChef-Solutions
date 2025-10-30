48c134c9499d85c0de2b9830c1dea076b91c53e6
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-30 23:18:39 +0530

    Fan Poll

---
+++ zero_to_1000/fizzbuzz2301.py
@@ -0,0 +1,8 @@
+# Question Link: https://www.codechef.com/problems/FIZZBUZZ2301
+
+# cook your dish here
+a, b, c = map(int, input().split())
+if a > b and a > c:
+    print("YES")
+else:
+    print("NO")
