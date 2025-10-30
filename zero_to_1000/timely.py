944b735a5e1037689e8f329eeea6392b7cb9f1ae
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-30 23:48:17 +0530

    Reach on Time

---
+++ zero_to_1000/timely.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/TIMELY
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if 30 <= x <= 60:
+        print("YES")
+    else:
+        print("NO")
