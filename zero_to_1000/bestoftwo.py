4c3059f10c91c72c161ec1c506f111b25b7c852e
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-10-31 00:19:22 +0530

    Best of Two

---
+++ zero_to_1000/bestoftwo.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/BESTOFTWO
+
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x > y:
+        print(x)
+    else:
+        print(y)
