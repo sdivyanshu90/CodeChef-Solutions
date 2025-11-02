d1a5fd775ad3f13b8439310de8c5f2b1fd543433
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-02 22:23:00 +0530

    Parity

---
+++ zero_to_1000/par2.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/PAR2
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
