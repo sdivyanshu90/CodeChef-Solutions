8b02c9fa97e5f5cd165906e2777ecf17ca09b564
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-02 20:52:24 +0530

    Candy Division

---
+++ zero_to_1000/candivide.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/CANDIVIDE
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n % 3 == 0:
+        print("YES")
+    else:
+        print("NO")
