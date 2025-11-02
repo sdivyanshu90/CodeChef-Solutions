19c812e87b051d17502ca261b8c60bde55977059
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-02 22:43:07 +0530

    Second Max of Three Numbers

---
+++ zero_to_1000/sndmax.py
@@ -0,0 +1,11 @@
+# Question Link: https://www.codechef.com/problems/SNDMAX
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if a > b and b > c or c > b and b > a:
+        print(b)
+    elif b > a and a > c or c > a and a > b:
+        print(a)
+    else:
+        print(c)
