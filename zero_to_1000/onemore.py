15c59cc4cda432c79bd94bfc594e530bfb20a096
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-04 23:06:02 +0530

    Just One More Episode

---
+++ zero_to_1000/onemore.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/ONEMORE
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x > 24:
+        print("Yes")
+    else:
+        print("No")
