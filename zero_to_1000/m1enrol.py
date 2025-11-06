49c00463fdd24f7682dc08c5f68c47dfa3c9573d
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-06 19:52:33 +0530

    MATH1 Enrolment

---
+++ zero_to_1000/m1enrol.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/M1ENROL
+
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if y - x > 0:
+        print(y - x)
+    else:
+        print(0)
