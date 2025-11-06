cea0cc55969a2b4970d9565313697f9fd5f68e2d
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-06 19:48:19 +0530

    Sleep Deprivation

---
+++ zero_to_1000/sleep.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/SLEEP
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x < 7:
+        print("YES")
+    else:
+        print("NO")
