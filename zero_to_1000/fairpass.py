24d4a4aca2606d1c19aa048625ac05b5495cbe23
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-06 19:33:52 +0530

    Passes for Fair

---
+++ zero_to_1000/fairpass.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/FAIRPASS
+
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if (x + 1) <= y:
+        print("YES")
+    else:
+        print("NO")
