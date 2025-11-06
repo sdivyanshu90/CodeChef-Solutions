04942e54d8a06db1c18a673c0729d0dc6631f2be
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-06 19:18:25 +0530

    Bidding

---
+++ zero_to_1000/auction.py
@@ -0,0 +1,11 @@
+# Question Link: https://www.codechef.com/problems/AUCTION
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if a > b and a > c:
+        print("Alice")
+    elif b > a and b > c:
+        print("Bob")
+    else:
+        print("Charlie")
