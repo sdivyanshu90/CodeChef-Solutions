a7235c53f83d7af7ccaee0841ad4b796c1461af1
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-06 19:46:07 +0530

    Fever

---
+++ zero_to_1000/fever.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/FEVER
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x > 98:
+        print("YES")
+    else:
+        print("NO")
