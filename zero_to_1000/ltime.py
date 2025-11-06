b32de82ca6cca98e743a7f245665821de6917c80
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-06 20:10:33 +0530

    Lunchtime

---
+++ zero_to_1000/ltime.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/LTIME
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if 1 <= x <= 4:
+        print("YES")
+    else:
+        print("NO")
