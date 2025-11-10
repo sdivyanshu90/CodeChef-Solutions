4f977bf5c63aa80c3bbab6b3fdc2e038bde71d09
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-10 16:37:19 +0530

    Access Code Equality

---
+++ zero_to_1000/wecnitk.py
@@ -0,0 +1,8 @@
+# Question Link: https://www.codechef.com/problems/WECNITK
+
+# cook your dish here
+a = input()
+if a == "WECNITK":
+    print("Welcome to Web Club!")
+else:
+    print("Access Denied")
