c83e7fdccb72d6be4ef63b7665d8f9d2ea300672
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-10 17:06:41 +0530

    Car Trip

---
+++ zero_to_1000/cartrip.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/CARTRIP
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n > 300:
+        print(n * 10)
+    else:
+        print(3000)
