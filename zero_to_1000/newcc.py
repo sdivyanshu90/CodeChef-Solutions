6354879faa531855e6580b88659e18629cf7233d
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-10 16:33:43 +0530

    All New CodeChef

---
+++ zero_to_1000/newcc.py
@@ -0,0 +1,10 @@
+# Question Link: https://www.codechef.com/problems/NEWCC
+
+# cook your dish here
+x, y = map(int, input().split())
+if x > y:
+    print("New")
+elif x < y:
+    print("Old")
+else:
+    print("Same")
