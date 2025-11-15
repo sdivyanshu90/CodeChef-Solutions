3d0755a5676524e5a97139c4595cc5b5c815001a
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-15 13:38:33 +0530

    Is it hot or cold

---
+++ zero_to_1000/hotcold.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/HOTCOLD
+
+# cook your dish here
+for _ in range(int(input())):
+    c = int(input())
+    if c > 20:
+        print("HOT")
+    else:
+        print("COLD")
