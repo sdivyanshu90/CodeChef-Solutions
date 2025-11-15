cc103d97d7648ca49e48a36e1a044f739162d14e
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-15 13:06:13 +0530

    The Gift

---
+++ zero_to_1000/cs2023_gift.py
@@ -0,0 +1,8 @@
+# Question Link: https://www.codechef.com/problems/CS2023_GIFT
+
+# cook your dish here
+x, n, m = map(int, input().split())
+if x + m >= n:
+    print("YES")
+else:
+    print("NO")
