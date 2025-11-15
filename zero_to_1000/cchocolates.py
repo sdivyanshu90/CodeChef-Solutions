9007b90adbf1abeb6690aefd390179cd36c035cb
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-15 13:28:47 +0530

    Chef and Chocolates

---
+++ zero_to_1000/cchocolates.py
@@ -0,0 +1,8 @@
+# Question Link: https://www.codechef.com/problems/CCHOCOLATES
+
+# cook your dish here
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    tot = 5 * x + 10 * y
+    print(tot // z)
+# Chef is buying chocolates for Chefina and I am searching how many chocolates he can buy with the given money. Sed Life.
