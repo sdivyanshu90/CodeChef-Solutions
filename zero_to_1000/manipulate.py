bdebec8bf95a59b3e6ea6abd271a006bed2afdd4
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-17 12:05:19 +0530

    Ezio and Guards

---
+++ zero_to_1000/manipulate.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/MANIPULATE
+
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x >= y:
+        print("YES")
+    else:
+        print("NO")
