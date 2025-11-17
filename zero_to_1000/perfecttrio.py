b963a082ae81a99deea78ccdabeb7b315a18ae71
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-17 13:01:17 +0530

    Perfect Trio

---
+++ zero_to_1000/perfecttrio.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/PERFECTTRIO
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if (a + b) == c or (b + c) == a or (a + c) == b:
+        print("YES")
+    else:
+        print("NO")
