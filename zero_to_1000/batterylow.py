57fc789134ae23056029372d27066e1f1afe2ebb
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-11-20 12:39:31 +0530

    Battery Low

---
+++ zero_to_1000/batterylow.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/BATTERYLOW
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n <= 15:
+        print("Yes")
+    else:
+        print("No")
