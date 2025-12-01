c1fea4b61be52956c8fa23e7c6f10fe2a1b27d0c
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-01 11:14:37 +0530

    Chef and Gym

---
+++ zero_to_1000/cgym.py
@@ -0,0 +1,11 @@
+# Question Link: https://www.codechef.com/problems/CGYM
+
+# cook your dish here
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    if x + y <= z:
+        print(2)
+    elif x <= z:
+        print(1)
+    else:
+        print(0)
