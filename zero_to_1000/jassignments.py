8c9b6422fae8350328283b9cfed5b78975e39eab
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-01 12:25:52 +0530

    Janmansh and Assignments

---
+++ zero_to_1000/jassignments.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/JASSIGNMENTS
+
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    if x + 3 <= 10:
+        print("Yes")
+    else:
+        print("No")
