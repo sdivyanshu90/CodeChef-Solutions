a75b28a1fdf7afd5c6b47e9aa082d71ac87bcc3c
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-01 11:36:14 +0530

    Greater Average

---
+++ zero_to_1000/avgproblem.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/AVGPROBLEM
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if (a + b) / 2 > c:
+        print("YES")
+    else:
+        print("NO")
