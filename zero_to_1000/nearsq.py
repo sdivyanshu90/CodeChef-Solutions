af7d146b58f1ce5ea1d2031f61115c8f32915207
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-03 16:47:13 +0530

    Nearest Square

---
+++ zero_to_1000/nearsq.py
@@ -0,0 +1,7 @@
+# Question Link: https://www.codechef.com/problems/NEARSQ
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    n_sqrt = int(n ** 0.5)
+    print(n_sqrt ** 2)
