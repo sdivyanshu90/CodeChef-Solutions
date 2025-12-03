8bd29c8156ee211a99eaac91779046fa3025b359
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-03 15:30:34 +0530

    Sale Season

---
+++ zero_to_1000/saleseason.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    discount = 0
+    if x <= 100:
+        discount = 0
+    elif 100 < x <= 1000:
+        discount = 25
+    elif 1000 < x <= 5000:
+        discount = 100
+    else:
+        discount = 500
+        
+    print(x - discount)
