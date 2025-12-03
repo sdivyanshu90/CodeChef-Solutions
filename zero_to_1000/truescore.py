de82580c2bd33d886206a36df2673061e5f94c9d
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-03 16:51:09 +0530

    Is the Score Consistent

---
+++ zero_to_1000/truescore.py
@@ -0,0 +1,11 @@
+# Question Link: https://www.codechef.com/problems/TRUESCORE
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+    
+    if c >= a and d >= b:
+        print("POSSIBLE")
+    else:
+        print("IMPOSSIBLE")
