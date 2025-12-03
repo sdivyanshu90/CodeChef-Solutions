4d520b4ae80e9a434705148fef5fac0065aef988
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-03 15:22:09 +0530

    Count Odd Even Divisors

---
+++ zero_to_1000/cntoddeven.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    odd, even = 0, 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            if i % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+                
+    print(odd, even)
