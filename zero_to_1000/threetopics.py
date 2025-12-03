3d70e23b113dd6f371ca5bf9efc6f35445ab660c
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-03 16:53:44 +0530

    The Three Topics

---
+++ zero_to_1000/threetopics.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/THREETOPICS
+
+# cook your dish here
+a, b, c, x = map(int, input().split())
+
+if x == a or x == b or x == c:
+    print("Yes")
+else:
+    print("No")
