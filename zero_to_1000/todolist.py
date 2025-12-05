731ff666fac5484dc592fa65b31c1a5f04819550
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-05 11:06:59 +0530

    Problems in your to-do list

---
+++ zero_to_1000/todolist.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    d = list(map(int, input().split()))
+    res = 0
+    for num in d:
+        if num >= 1000:
+            res += 1
+    print(res)
