7dfa072774d76beef2f0f41cc65a9f7423cbafbe
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-05 11:17:05 +0530

    Bowling Balls

---
+++ zero_to_1000/bowlball.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x, y = map(int, input().split())
+    a = list(map(int, input().split()))
+    res = 0
+    for i in range(len(a)):
+        if x <= a[i] <= y:
+            res += 1
+            
+    print(res)
