962ef8899bcb676218390c97a0a96b35d26ecb6a
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-07 11:28:04 +0530

    Breaking Sticks

--- zero_to_1000/brkstick.py
+++ zero_to_1000/brkstick.py
@@ -8,3 +8,10 @@ for _ in range(int(input())):
     for i in range(len(a)):
         res += (a[i] - 1)
     print(res)
+
+# Approach 2
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(sum(a) - n)
