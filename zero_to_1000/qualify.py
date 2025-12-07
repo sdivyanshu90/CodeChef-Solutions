fe13aaf880fe524266a92b1c73ac14ca5a5d90ea
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-07 11:14:49 +0530

    Qualify the round

---
+++ zero_to_1000/qualify.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, a, b = map(int, input().split())
+    tot = a + 2 * b
+    if tot >= n:
+        print("Qualify")
+    else:
+        print("NotQualify")
