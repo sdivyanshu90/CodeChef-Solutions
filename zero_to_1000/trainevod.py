a5b8038175bd2d6166f730cedddfb041f669fdad
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-07 11:32:51 +0530

    Train Even or Odd

---
+++ zero_to_1000/trainevod.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    odd, even = 0, 0
+
+    for i in range(n):
+        if i % 2 == 0:
+            even += a[i]
+        else:
+            odd += a[i]
+            
+    print(max(odd, even))
