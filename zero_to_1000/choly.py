ca02b44a0659b8186eb65bd1584b64dc4fcc9aa0
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-09 13:44:33 +0530

    Chess Olympiad

---
+++ zero_to_1000/choly.py
@@ -0,0 +1,7 @@
+win, draw, loss = map(int, input().split())
+first = 1 * win + draw * 0.5
+second = 1 * loss + draw * 0.5
+if first +  (4 - (win + draw + loss)) > second:
+    print("Yes")
+else:
+    print("No")
