50c2cb1b18b2bdc9fafd2ec4145a11bda07cbad2
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-09 11:58:25 +0530

    Maximise the Tastiness

---
+++ zero_to_1000/maxtaste.py
@@ -0,0 +1,6 @@
+# Question Link: https://www.codechef.com/problems/MAXTASTE
+
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    print(max(a, b) + max(c, d))
