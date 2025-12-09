ab94e6323ecfa29c2fcc75212038a2a362da49e1
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-09 11:54:22 +0530

    Determine the Winner

---
+++ zero_to_1000/winnerr.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    pa, pb, qa, qb = map(int, input().split())
+    p = max(pa, pb)
+    q = max(qa, qb)
+    if p > q:
+        print("Q")
+    elif q > p:
+        print("P")
+    else:
+        print("TIE")
