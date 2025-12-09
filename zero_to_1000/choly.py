5ac48e9d9dfc6f5280a821da72baeec1acfc02b7
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-09 13:44:59 +0530

    Chess Olympiad

--- zero_to_1000/choly.py
+++ zero_to_1000/choly.py
@@ -1,3 +1,6 @@
+# Question Link: https://www.codechef.com/problems/CHOLY
+
+# cook your dish here
 win, draw, loss = map(int, input().split())
 first = 1 * win + draw * 0.5
 second = 1 * loss + draw * 0.5
