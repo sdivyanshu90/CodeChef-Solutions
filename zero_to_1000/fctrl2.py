90fd88245c9c8223d41c56aa3680b051a7a0d38e
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-11 11:38:48 +0530

    Small factorials

---
+++ zero_to_1000/fctrl2.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] * i
+        
+    print(dp[n])
