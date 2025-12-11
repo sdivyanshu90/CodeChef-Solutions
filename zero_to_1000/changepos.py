10c5a19d774a2c7ee18d8f42a81adc083b8f2423
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-11 12:39:16 +0530

    Change Row and Column Both

---
+++ zero_to_1000/changepos.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/CHANGEPOS
+
+# cook your dish here
+for _ in range(int(input())):
+    sx, sy, ex, ey = map(int, input().split())
+    if sx != ex and sy != ey:
+        print(1)
+    else:
+        print(2)
