fc0cc55bfbd8e3957156cc7e2ff7789db32a9f12
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-11 11:52:08 +0530

    Mario and Bullet

---
+++ zero_to_1000/bullet.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    a = y // x
+    diff = z - a
+    if diff < 0:
+        print(0)
+    else:
+        print(diff)
