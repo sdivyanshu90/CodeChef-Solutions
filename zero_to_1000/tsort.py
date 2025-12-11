2b036270425f9b133b1163b8bc9414b206762e27
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-11 12:58:04 +0530

    Simple Sorting

---
+++ zero_to_1000/tsort.py
@@ -0,0 +1,12 @@
+# Question Link: https://www.codechef.com/problems/TSORT
+
+# cook your dish here
+lis=[]
+t=int(input())
+while t>0:
+    n=int(input())
+    lis.append(n)
+    t-=1
+lis.sort()
+for i in range(0,len(lis)):
+    print(lis[i])
