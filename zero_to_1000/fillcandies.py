c6fd70b07db30f1bd91c379197397e4941a1d70b
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-13 12:10:13 +0530

    Fill Candies

---
+++ zero_to_1000/fillcandies.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    candies, pockets, pocket_capacity = map(int, input().split())
+    # print(f"Candies: {candies}, No. of Pockets: {pockets}, Each Pocket Candy Capacity: {pocket_capacity}")
+    part1 = pockets * pocket_capacity
+    if candies % part1 == 0:
+        print(candies // part1)
+    else:
+        print(candies // part1 + 1)
