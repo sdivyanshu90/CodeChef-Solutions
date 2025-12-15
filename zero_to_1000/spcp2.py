93352b5ae0454abdbdedbf90b392a84b205188d1
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-15 12:26:17 +0530

    Airlines

---
+++ zero_to_1000/spcp2.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    req = 0
+    if x % 100 == 0:
+        req = x // 100
+    else:
+        req = x // 100 + 1
+    if n > req:
+        print(0)
+    else:
+        print(req - n)
