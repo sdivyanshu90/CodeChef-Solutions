83eebbc3887c8cb0d897ff377b7c24625f27b4e5
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-15 12:15:52 +0530

    Legs on a Farm

---
+++ zero_to_1000/farmlegs.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    res = 0
+    while n > 0:
+        if n >= 4:
+            n -= 4
+        else:
+            n -= 2
+        res += 1
+        
+    print(res)
