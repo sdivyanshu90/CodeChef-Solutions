d088e60d99cc9831b0f682692958ff4cf1829e3a
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-15 11:23:05 +0530

    Weights

---
+++ zero_to_1000/wghts.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x, y, z = map(int, input().split())
+    if n == x or n == y or n == z or n == (x + y) or n == (y + z) or n == (x + z) or n == (x + y + z):
+        print("YES")
+    else:
+        print("NO")
