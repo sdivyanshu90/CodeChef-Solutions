fe95baed3abe26d6debd5551da72b2b56d90bf12
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-17 13:57:43 +0530

    Self Defence Training

---
+++ zero_to_1000/selfdef.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    res = 0
+    for num in a:
+        if 10 <= num <= 60:
+            res += 1
+    print(res)
