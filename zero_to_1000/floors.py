eba5a3f5a8d194d4c10ec5dc896b7997f9800a73
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-17 14:00:30 +0530

    Too many Floors

---
+++ zero_to_1000/floors.py
@@ -0,0 +1,6 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    floor_a = (a - 1) // 10 + 1
+    floor_b = (b - 1) // 10 + 1
+    print(abs(floor_a - floor_b))
