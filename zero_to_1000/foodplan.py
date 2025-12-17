d2b5fa8e7eb9665b99bcca520d764d2107e6d82b
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-17 13:56:29 +0530

    Online or Offline

---
+++ zero_to_1000/foodplan.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    online, offline = map(int, input().split())
+    final_price = online - (0.1 * online)
+    if final_price < offline:
+        print("ONLINE")
+    elif final_price > offline:
+        print("DINING")
+    else:
+        print("EITHER")
