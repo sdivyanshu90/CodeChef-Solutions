406ec94cbb4e810cbb7c2ef7dc7b6f0d24a2ae12
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-17 14:52:48 +0530

    Long Queue

---
+++ zero_to_1000/longqueue.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    pos = len(a) - 1
+    while pos > 0 and a[pos - 1] <= a[pos] // 2:
+        a.pop(pos - 1)
+        pos -= 1
+    print(pos + 1)
