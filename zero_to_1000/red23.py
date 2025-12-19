52f3a779d0a0fea4cba7eb5ee9f18e4f5c7249c3
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-19 11:57:17 +0530

    Number Reduction

---
+++ zero_to_1000/red23.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    while n > 0:
+        if n % 2 == 0:
+            n //= 2
+        elif n > 3:
+            n -= 3
+        else:
+            break
+    print(n)
