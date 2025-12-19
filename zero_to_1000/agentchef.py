183769846de4800a9e6789e31449b67c40ec344e
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-19 12:34:28 +0530

    Selling Insurance

---
+++ zero_to_1000/agentchef.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = 0.2 * n
+    if 100 % a == 0:
+        print(int(100 // a))
+    else:
+        print(int((100 // a) + 1))
