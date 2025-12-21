c4b281aae24413c1a9ff02d80eb13742b5955247
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-21 14:43:55 +0530

    Chef and Races

---
+++ zero_to_1000/chefraces.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    x = set(map(int, input().split()))
+    if len(x) == 4:
+        print(2)
+    elif len(x) == 3:
+        print(1)
+    else:
+        print(0)
