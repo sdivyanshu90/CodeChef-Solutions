f62296b1dc48cdb205dfaab79e151110a44e7290
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-21 15:19:05 +0530

    Rectangled

--- zero_to_1000/rctgld.py
+++ zero_to_1000/rctgld.py
@@ -6,3 +6,14 @@ for _ in range(int(input())):
     width = (n // 4)
     length = (n - (2 * width)) // 2
     print(width * length)
+
+# Approach 2
+# for _ in range(int(input())):
+#     n = int(input())
+#     max_area = 0
+#     for w in range(1, n//2):
+#         l = (n - 2*w) // 2
+#         area = w * l
+#         if area > max_area:
+#             max_area = area
+#     print(max_area)
