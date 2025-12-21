83cbdea9c23f9ddef34907184563063ba5239096
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-21 14:15:20 +0530

    Count the ACs

---
+++ zero_to_1000/acs.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    res = sum(divmod(n, 100))
+    # print(res)
+    if res <= 10:
+        print(res)
+    else:
+        print(-1)
