7c1918a3684f9e5e2ccf0948ed3dd624d4fb317c
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-21 14:26:08 +0530

    Building Race

---
+++ zero_to_1000/buildingrace.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    chef_floor, chefina_floor, chef_speed, chefina_speed = map(int, input().split())
+    chef_min = chef_floor / chef_speed
+    chefina_min = chefina_floor / chefina_speed
+    if chef_min > chefina_min:
+        print("Chefina")
+    elif chefina_min > chef_min:
+        print("Chef")
+    else:
+        print("Both")
