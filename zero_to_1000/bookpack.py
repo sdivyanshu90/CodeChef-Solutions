1573dff561f630fa503497819aa496798cc4548f
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-25 11:53:19 +0530

    Packing Books

---
+++ zero_to_1000/bookpack.py
@@ -0,0 +1,10 @@
+# cook your dish here
+from math import ceil
+
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    if y > z:
+        need = ceil(y / z)
+        print(x * need)
+    else:
+        print(x)
