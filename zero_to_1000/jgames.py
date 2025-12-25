1e83fdb867f7a8caf306b77a0b249dfdde01bebc
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-25 11:28:23 +0530

    Janmansh and Games

---
+++ zero_to_1000/jgames.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if (x % 2 == 0 and y % 2 == 0) or (x % 2 != 0 and y % 2 != 0):
+        print("Janmansh")
+    else:
+        print("Jay")
