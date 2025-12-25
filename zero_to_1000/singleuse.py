f101827e331b2d0194545597f27cf66dd2f3db7f
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-25 12:13:38 +0530

    Single-use Attack

---
+++ zero_to_1000/singleuse.py
@@ -0,0 +1,10 @@
+# cook your dish here
+from math import ceil
+
+for _ in range(int(input())):
+    x, y, h = map(int, input().split())
+    rem = x - h
+    res = 1
+    if rem > 0:
+        res += ceil(rem / y)
+    print(res)
