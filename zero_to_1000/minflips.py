9294b4aa4799d548ad084ca653029a8ccfa3d33f
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-27 12:22:44 +0530

    Minimum number of Flips

---
+++ zero_to_1000/minflips.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    v = list(map(int, input().split()))
+    cnt1 = sum(1 for i in range(n) if v[i] == 1)
+    if n % 2 == 1:
+        print(-1)
+    elif cnt1 >= n // 2:
+        print(cnt1 - n // 2)
+    else:
+        print(n // 2 - cnt1)
