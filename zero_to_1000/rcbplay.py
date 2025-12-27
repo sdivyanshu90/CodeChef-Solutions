677bbfe61a335493bdb44c228cb591b86e7ccc14
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-27 14:37:09 +0530

    RCB and Playoffs

---
+++ zero_to_1000/rcbplay.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    score = x + (z * 2)
+    if score >= y:
+        print("YES")
+    else:
+        print("NO")
