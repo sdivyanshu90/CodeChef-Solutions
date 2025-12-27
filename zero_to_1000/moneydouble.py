424eda5edf7da5e29d3e118d5bf7af09325d64b2
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-27 14:10:48 +0530

    Money Double

---
+++ zero_to_1000/moneydouble.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x < 1000:
+        x += 1000
+        print((2 ** (y - 1))* x)
+    else:
+        print((2 ** y) * x)
