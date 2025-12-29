8d5b0f369ef7028c8400649269a8f9aac045627a
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-29 12:21:42 +0530

    Primality Test

---
+++ zero_to_1000/prb01.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    prime = True
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            prime = False
+            break
+    
+    if prime and n != 1:
+        print("yes")
+    else:
+        print("no")
