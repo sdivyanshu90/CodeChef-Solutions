b3c079a5b5c9d7338d676e9d0a8066073ad15895
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-29 12:39:55 +0530

    Cost of Groceries

---
+++ zero_to_1000/kitchencost.py
@@ -0,0 +1,8 @@
+class Solution:
+    def compute(self, n, x, a, b):
+        # write your code here 
+        res = 0
+        for i in range(n):
+            if a[i] >= x:
+                res += b[i]
+        return res
