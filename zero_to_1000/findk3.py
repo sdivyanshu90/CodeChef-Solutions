dd3ea2a4e5da85f1856ef4cc03f2cffdc4a74a27
sdivyanshu90 <divyanshu74.80@gmail.com>
2025-12-31 12:14:54 +0530

    FIND A and B

---
+++ zero_to_1000/findk3.py
@@ -0,0 +1,19 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    
+    possible_pairs = [
+        (x * y, z),
+        (x * z, y),
+        (y * z, x),
+    ]
+    
+    found = False
+    for a, b in possible_pairs:
+        if a % b == 0:
+            print(a, b)
+            found = True
+            break
+    
+    if not found:
+        print(-1)
