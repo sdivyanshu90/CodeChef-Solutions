a6ea74913bc0d8e79a3cbd2bfaa2305743e79a27
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-02 11:43:16 +0530

    Nearest Court

---
+++ zero_to_1000/nearestcourt.py
@@ -0,0 +1,9 @@
+# Question Link: https://www.codechef.com/problems/NEARESTCOUNT
+
+# cook your dish here
+from math import ceil
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    diff = abs(x - y)
+    print(ceil(diff / 2))
