4a5fb8cf9c18b760f71c12eac5fe852954afcaf6
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-02 12:15:01 +0530

    Butterfly

---
+++ zero_to_1000/bfly.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    r, g, b = map(int, input().split())
+    if r > (g + b) or g > (r + b) or b > (g + r):
+        print("NO")
+    else:
+        print("YES")
