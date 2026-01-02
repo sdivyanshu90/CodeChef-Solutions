32ae8d550a2e8006cb0afbb763d03f61131c85ec
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-02 12:46:25 +0530

    Snape and Ladder

---
+++ zero_to_1000/snape.py
@@ -0,0 +1,6 @@
+# cook your dish here
+for _ in range(int(input())):
+    b, ls = map(int, input().split())
+    mini = ((b ** 2) + (ls ** 2)) ** 0.5
+    maxi = ((abs((b ** 2) - (ls ** 2))) ** 0.5)
+    print(maxi, mini)
