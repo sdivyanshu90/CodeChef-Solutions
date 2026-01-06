8e7fd3bfd5f27757b56678799107700c51627c01
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-06 12:47:12 +0530

    Advitiya

---
+++ zero_to_1000/advitiya.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    s = input()
+    target = 'ADVITIYA'
+    res = 0
+    for i in range(8):
+        if s[i] != target[i]:
+            if ord(s[i]) < ord(target[i]):
+                res += (ord(target[i]) - ord(s[i]))
+            else:
+                res += (26 - (ord(s[i]) - ord(target[i])))
+    print(res)
