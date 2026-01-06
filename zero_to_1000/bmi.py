ecb89e28b5bea436c694e291b689228b80d0f7ba
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-06 12:53:48 +0530

    Body Mass Index

---
+++ zero_to_1000/bmi.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    m, h = map(int, input().split())
+    bmi = m // (h ** 2)
+    if bmi <= 18:
+        print(1)
+    elif 19 <= bmi <= 24:
+        print(2)
+    elif 25 <= bmi <= 29:
+        print(3)
+    else:
+        print(4)
