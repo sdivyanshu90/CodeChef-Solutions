d25e716380b4eb0d97294a6790f430585b1fbd1a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-06 13:09:27 +0530

    Id and Ship

---
+++ zero_to_1000/flow010.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    s = input()
+    if s == "B" or s == "b":
+        print("BattleShip")
+    elif s == "C" or s == "c":
+        print("Cruiser")
+    elif s == "D" or s == "d":
+        print("Destroyer")
+    else:
+        print("Frigate")
