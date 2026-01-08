5fa79c2378ee1207bb358a8a6267594101d766d2
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-08 15:21:48 +0530

    Chef and Vacation Transportation

---
+++ zero_to_1000/cheftrans.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y, z = map(int, input().split())
+    if x + y < z:
+        print("PLANEBUS")
+    elif x + y > z:
+        print("TRAIN")
+    else:
+        print("EQUAL")
