b93133276ebc3659db2c9d9e923e95bdf00238a4
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-08 15:25:50 +0530

    Passing Marks

---
+++ zero_to_1000/cutoff.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    # your code goes here
+    a.sort(reverse = True)
+    if x == n:
+        print(min(a) - 1)
+    else:
+        print(a[x - 1] - 1)
