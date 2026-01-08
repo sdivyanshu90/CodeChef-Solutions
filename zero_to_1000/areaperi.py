5f0050204f7e4770e99332b28beb9c7729ec0f1e
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-08 15:36:31 +0530

    Area OR Perimeter

---
+++ zero_to_1000/areaperi.py
@@ -0,0 +1,16 @@
+# cook your dish here
+a = int(input())
+b = int(input())
+
+area = a * b
+peri = 2 * (a + b)
+
+if area > peri:
+    print("Area")
+    print(area)
+elif peri > area:
+    print("Peri")
+    print(peri)
+else:
+    print("Eq")
+    print(area)
