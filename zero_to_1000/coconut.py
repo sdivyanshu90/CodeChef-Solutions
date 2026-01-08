ed6eab2af9c1ba82dc3841b91b83aef23cbdf78d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-08 13:24:41 +0530

    Summer Heat

---
+++ zero_to_1000/coconut.py
@@ -0,0 +1,6 @@
+# cook your dish here
+for _ in range(int(input())):
+    xa, xb, needa, needb = map(int, input().split())
+    typea = needa // xa
+    typeb = needb // xb
+    print(typea +  typeb)
