a06c2df666ced81124a846ff7092ed35b64095db
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-10 11:56:56 +0530

    Alternate Additions

---
+++ zero_to_1000/alteradd.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    
+    diff = b - a
+    if diff % 3 == 2:
+        print("NO")
+    else:
+        print("YES")
