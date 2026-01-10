cec0bf8f36a77f7d62d5e7955140f840af2ca7ae
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-10 11:34:55 +0530

    Marble Collector

---
+++ zero_to_1000/marbcoll.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    set_a = set(a)
+    not_collected = list(range(1, m + 1))
+    res = len(not_collected)
+    for item in set_a:
+        if item in not_collected:
+            res -= 1
+    print(res)
