dfe9268b483ab0f6b0fcb163da8a4159075fd405
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-10 11:33:44 +0530

    Construct N

---
+++ zero_to_1000/conn.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n < 8:
+        if n % 2 == 0 or n == 7:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("YES")
