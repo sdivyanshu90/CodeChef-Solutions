539e4901b9fda29913a850d083aaa0337b453f35
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-10 11:44:43 +0530

    Car Choice

---
+++ zero_to_1000/carchoice.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    x1, x2, y1, y2 = map(int, input().split())
+    car1 = y1 / x1
+    car2 = y2 / x2
+    if car1 > car2:
+        print(1)
+    elif car2 > car1:
+        print(-1)
+    else:
+        print(0)
