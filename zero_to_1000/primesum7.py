cf74d6fe3111c14b1c49dd560a4f239229e1ce37
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-12 10:56:02 +0530

    Prime Sum

---
+++ zero_to_1000/primesum7.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(str, input().split()))
+    c1, c2, c3 = a.count('1'), a.count('2'), a.count('3')
+    part1 = (c1 * (c1 - 1)) // 2
+    part2 = c1 * c2 + c2 * c3
+    print(part1 + part2)
