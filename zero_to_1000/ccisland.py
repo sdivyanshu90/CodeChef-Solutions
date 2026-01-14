1b289c20c5219bbc7190f2d25c94fb30b85487a5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-14 11:43:58 +0530

    Chef On Island

---
+++ zero_to_1000/ccisland.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y, xr, yr, d = map(int, input().split())
+    food = x / xr
+    water = y / yr
+    mini = min(food, water)
+    if mini >= d:
+        print("YES")
+    else:
+        print("NO")
