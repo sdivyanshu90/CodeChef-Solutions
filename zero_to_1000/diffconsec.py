11e3892dec53125c8e86ed3a9a3ddb8d29485432
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-14 12:04:36 +0530

    Different Consecutive Characters

---
+++ zero_to_1000/diffconsec.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    count = 0
+    for i in range(n - 1):
+        if s[i] == s[i + 1]:
+            count += 1
+    print(count)
