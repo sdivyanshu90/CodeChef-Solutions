eebc36c21034b6304cae6680f285520409ddb9ba
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-14 12:47:04 +0530

    Secret Recipe

---
+++ zero_to_1000/chefrun.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    x1, x2, x3, v1, v2 = map(int, input().split())
+    kefa = (x2 - x3) / v2
+    chef = (x3 - x1) / v1
+    if chef > kefa:
+        print("Kefa")
+    elif chef < kefa:
+        print("Chef")
+    else:
+        print("Draw")
