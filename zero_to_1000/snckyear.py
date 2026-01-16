2edd4042182ecc48d8bc31ca9984a5d07e7b758c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-16 11:31:33 +0530

    Chef and SnackDown

---
+++ zero_to_1000/snckyear.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n in [2010, 2015, 2016, 2017, 2019]:
+        print("HOSTED")
+    else:
+        print("NOT HOSTED")
