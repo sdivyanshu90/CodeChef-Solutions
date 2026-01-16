504db3120980269569f0a1491b35b6a2e4d39f50
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-16 11:16:09 +0530

    More Cookies

---
+++ zero_to_1000/morecook.py
@@ -0,0 +1,17 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, c = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    a_set = set(a)
+    if c not in a_set and any(i < c for i in a):
+        print(0)
+        continue
+    
+    res = 1
+    while True:
+        temp = c + res
+        if temp not in a_set and any(i < temp for i in a):
+            print(res)
+            break
+        res += 1
