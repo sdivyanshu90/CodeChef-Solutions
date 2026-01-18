af644c16c7c93ee73bb93b058c4da6ae48275d2a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-18 12:20:34 +0530

    Running Comparison

---
+++ zero_to_1000/runcompare.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    alice = list(map(int, input().split()))
+    bob = list(map(int, input().split()))
+    res = 0
+    for i in range(n):
+        if alice[i] > 2*bob[i] or bob[i] > 2*alice[i]:
+            continue
+        else:
+            res += 1
+    print(res)
