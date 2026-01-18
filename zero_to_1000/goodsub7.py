4f0f9c997852dbc203648333a91d9e2d1c23ca9c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-18 12:19:18 +0530

    Good Subsequence

---
+++ zero_to_1000/goodsub7.py
@@ -0,0 +1,19 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    if n == 0:
+        print(0)
+        continue
+    
+    count = 1
+    last_parity = a[0] % 2
+    
+    for i in range(1, n):
+        current_parity = a[i] % 2
+        if current_parity != last_parity:
+            count += 1
+            last_parity = current_parity
+            
+    print(count)
