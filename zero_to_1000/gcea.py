6f2267af13b99e855af4b543a83535b56b2b7bea
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-18 12:18:42 +0530

    Gotta Catch Em All

---
+++ zero_to_1000/gcea.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x, y = map(int, input().split())
+    a = list(map(int, input().split()))
+    master = 0
+    normal = 0
+    res = 0
+    for poke in a:
+        normal = (x*poke)
+        master = y
+        res += min(normal, master)
+            
+    print(res)
