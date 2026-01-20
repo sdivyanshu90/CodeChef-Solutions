d29d9be74b134c37c83fe9f6e128f016435e1eee
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-20 11:24:52 +0530

    Two Ranges

---
+++ zero_to_1000/tworanges.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    mini = min(a, b, c, d)
+    maxi = max(a, b, c, d)
+    res = 0
+    for i in range(mini, maxi + 1):
+        if a <= i <= b or c <= i <= d:
+            res += 1
+    print(res)
