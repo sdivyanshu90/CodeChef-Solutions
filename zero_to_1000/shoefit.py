dd9b98497ea0bb3a4589b76fb42eaba938226abb
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-20 11:44:10 +0530

    Shoe Fit

---
+++ zero_to_1000/shoefit.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for _ in range(int(input())):
+    a = list(map(int, input().split()))
+    zero = 0
+    one = 0
+    for num in a:
+        if num == 0:
+            zero += 1
+        else:
+            one += 1
+    if zero >= 1 and one >= 1:
+        print(1)
+    else:
+        print(0)
