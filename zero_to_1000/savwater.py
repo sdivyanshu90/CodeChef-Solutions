20404e30e875cb2cf71dff237f114a5bc5455e82
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-20 11:20:11 +0530

    A - Save Water Save Life

---
+++ zero_to_1000/savwater.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    h, x, y, c = map(int, input().split())
+    grey =  x + y // 2
+    water = h * grey
+    if c >= water:
+        print("YES")
+    else:
+        print("NO")
