836a557dbd5f7056c452cb80bfa36f096a72c424
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-22 12:56:10 +0530

    No Winner

---
+++ zero_to_1000/nowinner.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c, m = map(int, input().split())
+    scores = sorted([a,b,c])
+    a, b, c = scores[0], scores[1], scores[2]
+    if (a + m >= b) or (b + m >= c):
+        print("YES")
+    else:
+        print("NO")
