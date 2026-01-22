fa7ac84bdbeb12023c4b88db46d229d3130ac408
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-22 12:33:03 +0530

    Difficulty Rating Order

---
+++ zero_to_1000/ratinginprac.py
@@ -0,0 +1,15 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    flag = True
+    for i in range(n - 1):
+        if a[i + 1] >= a[i]:
+            continue
+        else:
+            flag = False
+            break
+    if flag:
+        print("Yes")
+    else:
+        print("No")
