a5f8e74ad5cc88bca69cd430e952d3362090653f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-22 12:37:31 +0530

    Chef and Price Control

---
+++ zero_to_1000/pricecon.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    p = list(map(int, input().split()))
+    initial = sum(p)
+    for i in range(n):
+        if p[i] > k:
+            p[i] = k
+    final = sum(p)
+    print(initial - final)
