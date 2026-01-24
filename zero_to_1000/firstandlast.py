e12ad9c7687e57a6ba9244718ce65192a203342f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-24 11:04:48 +0530

    First and Last

---
+++ zero_to_1000/firstandlast.py
@@ -0,0 +1,4 @@
+# cook your dish here
+for _ in range(int(input())):
+    N, A = int(input()), list(map(int, input().split()))
+    print(max(A[0] + A[-1], max(A[i] + A[i+1] for i in range(N - 1))))
