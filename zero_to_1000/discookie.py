1d1d725ba1c0bb38ab0c3899da4a5842ad739bbb
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-24 10:36:05 +0530

    Distribute Cookies

---
+++ zero_to_1000/discookie.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+
+    if m < n:
+        print(n - m)
+    else:
+        lower = (m // n) * n
+        upper = lower + n
+        print(min(m - lower, upper - m))
