e9f05818c569a2e0b55a3f61cc57a7fc3e340ea8
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-26 16:09:41 +0530

    Make Subarray

---
+++ zero_to_1000/makesub.py
@@ -0,0 +1,21 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    idx = []
+    for i in range(n):
+        if s[i] == "1":
+            idx.append(i)
+    # print(idx)
+    res = 0
+    if len(idx) > 1:
+        mini = idx[0]
+        maxi = idx[-1]
+        for i in range(mini, maxi + 1):
+            if i in idx:
+                continue
+            else:
+                res += 1
+        print(res)
+    else:
+        print(res)
