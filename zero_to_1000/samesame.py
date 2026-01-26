af6328d4af131790bfcd1b497bf7cda1fc299b92
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-26 16:48:25 +0530

    Nearly Equal

--- zero_to_1000/samesame.py
+++ zero_to_1000/samesame.py
@@ -1,3 +1,5 @@
+# Question Link: https://www.codechef.com/problems/SAMESAME
+
 # cook your dish here
 for _ in range(int(input())):
     n, m = map(int, input().split())
@@ -7,11 +9,11 @@ for _ in range(int(input())):
     for i in range(n - m + 1):
         substrings.append(a[i:i+m])
     # print(substrings)
-    res = []
+    res = float('inf')
     for sub in substrings:
         temp = 0
         for i in range(m):
             if sub[i] != b[i]:
                 temp += 1
-        res.append(temp)
-    print(min(res))
+        res = min(temp, res)
+    print(res)
