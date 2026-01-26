79e4cbe3b0a9ff52b05faeb5339f9b1b9b67ad89
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-26 16:12:35 +0530

    Maximum Ones

---
+++ zero_to_1000/mxon.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    s = list(input())
+    
+    for i in range(n - 2, -1, -1):
+        if k == 0:
+            break
+        if s[i] == '0' and s[i+1] == '1':
+            s[i] = '1'
+            k -= 1
+
+    print(s.count('1'))
