86e25e77a239a9eb05d30efaad4366aaf0e9e9ed
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-26 16:57:06 +0530

    Non-matching Name

---
+++ zero_to_1000/abnomat.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    sa = set(input())
+    sb = set(input())
+    
+    if len(sa | sb) == 26:
+        print("NO")
+    else:
+        print("YES")
