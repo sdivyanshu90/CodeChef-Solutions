62f3d4a224bf37b5b8e74bddf42d19f79e38211d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-29 16:03:57 +0530

    Rearranging digits to get a multiple of 5

---
+++ zero_to_1000/digarr.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    zero, five = s.count('0'), s.count("5")
+    if zero >= 1 or five >= 1:
+        print("YES")
+    else:
+        print("No")
