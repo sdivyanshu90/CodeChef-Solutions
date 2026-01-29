fb18877fc15cbb5957207cd1f3b62a60f43f5dca
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-29 16:17:21 +0530

    Range Minimize

---
+++ zero_to_1000/mnr.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    ans = float("inf")
+    for s in [n, n - 1, n - 2]:
+        for i in range(n - s + 1):
+            ans = min(ans, a[i + s - 1] - a[i])
+    print(ans)
