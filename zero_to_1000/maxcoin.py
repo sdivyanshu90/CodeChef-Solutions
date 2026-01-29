77b877aa31a53502332e57051ebc6df534370533
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-29 16:25:31 +0530

    Maximum Coins

---
+++ zero_to_1000/maxcoin.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    
+    gain = (2 ** (n + 1)) - (2 ** (n - x + 1))
+    loss = (2 ** (n - x + 1)) - 2
+    
+    print(gain - loss)
