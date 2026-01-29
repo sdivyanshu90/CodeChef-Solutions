f6ab8f066f29340aa38ed4dc5b6bc2ded1f884ed
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-29 15:56:12 +0530

    Non-Negative Product

---
+++ zero_to_1000/nonnegprod.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    prod = 1
+    for num in a:
+        prod *= num
+    # print(prod)
+    if prod >= 0:
+        print(0)
+    else:
+        print(1)
