cea98da9d93b814683d84df027ca5948c11df9f3
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-29 16:29:49 +0530

    Chef Bakes Cake 3

---
+++ zero_to_1000/bakecake3.py
@@ -0,0 +1,15 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    max_profit = 0
+
+    for x in range(0, max(a) + 1):
+        temp = 0
+        for customers in a:
+            temp += min(x, customers)
+        profit = temp * 50 - x * n * 30
+        max_profit = max(max_profit, profit)
+    
+    print(max_profit)
