a9246eef6857b4839646086e1e9febbc806aa11c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-29 15:53:59 +0530

    Janmansh at Fruit Market

---
+++ zero_to_1000/jmarket.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, a, b, c = map(int, input().split())
+    mini = min(a, b, c)
+    second_mini = 0
+    if a <= b <= c or c <= b <= a:
+        second_mini = b
+    elif b <= a <= c or c <= a <= b:
+        second_mini = a
+    else:
+        second_mini = c
+    print(mini * (x - 1) + second_mini)
