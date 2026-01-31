0e07c041da478b8b3de67bf82c384d88066f388e
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-31 13:26:09 +0530

    Chef and Cook-Off

---
+++ zero_to_1000/ccook.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for i in range(int(input())):
+    p = list(map(int, input().split()))
+    s = {
+        0: "Beginner",
+        1: "Junior Developer",
+        2: "Middle Developer",
+        3: "Senior Developer",
+        4: "Hacker",
+        5: "Jeff Dean"
+    }
+    add = sum(p)
+    print(s[add])
