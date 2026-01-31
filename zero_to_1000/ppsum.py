202dd821d40a6a17897d223d9c694c4e2fbd821e
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-31 13:24:43 +0530

    Puppy and Sum

---
+++ zero_to_1000/ppsum.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    d, n = map(int, input().split())
+    current = n
+
+    for _ in range(d):
+        current = current * (current + 1) // 2
+
+    print(current)
