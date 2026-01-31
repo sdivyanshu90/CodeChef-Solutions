98f671d60ca885d9f3997241b5e0a753b26f59c5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-31 13:17:13 +0530

    Two Rooks

---
+++ zero_to_1000/tworooks.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    x1, x2, y1, y2 = map(int, input().split())
+    if x1 == y1 or x2 == y2:
+        print("YES")
+    else:
+        print("NO")
