a236f56bffb411514a3a5d69a13474b8758557e5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-01-31 13:10:31 +0530

    Technex Tickets

---
+++ zero_to_1000/p2_175.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n == 1 or n == 3:
+        print(1)
+    elif n % 2 == 0:
+        print(n // 2 + 1)
+    else:
+        print(n // 2)
