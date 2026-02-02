86dbd93f1075345aa8acf597ece0ea58a4327523
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-02 12:41:22 +0530

    Chef and Lockout Draws

---
+++ zero_to_1000/lockdraw.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if a + b == c or b + c == a or a + c == b:
+        print("YES")
+    else:
+        print("NO")
