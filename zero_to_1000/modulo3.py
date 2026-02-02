4236aca4e3f9fb744e1836bbf883beed02213c3e
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-02 12:28:03 +0530

    Divisible by 3

---
+++ zero_to_1000/modulo3.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    count = 0
+    while (a % 3 != 0) and (b % 3 != 0):
+        a, b = abs(a - b), min(a, b)
+        count += 1
+    print(count)
