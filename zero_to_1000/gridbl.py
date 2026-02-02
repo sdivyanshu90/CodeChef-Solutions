4f354e6ec52efb9f73ccfaa7c367b76b26d29a74
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-02 12:14:07 +0530

    Fill The Grid

---
+++ zero_to_1000/gridbl.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    if n % 2 == 0 and m % 2 == 0:
+        print(0)
+    elif n % 2 == 0:
+        print(n)
+    elif m % 2 == 0:
+        print(m)
+    else:
+        print(m + n - 1)
