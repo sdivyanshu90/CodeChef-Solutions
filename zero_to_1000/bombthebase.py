134d9da6bb0a90003fb0d59bcf6e899d3d641a41
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-04 10:59:53 +0530

    Bomb the base

---
+++ zero_to_1000/bombthebase.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    res = 0
+    for i in range(n):
+        if a[i] < x:
+            res = i + 1
+
+    print(res)
