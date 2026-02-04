091094f2c663165c9596a0571978c69f35741cee
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-04 11:02:36 +0530

    Fit Squares in Triangle

---
+++ zero_to_1000/trisq.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n < 4:
+        print(0)
+    else:
+        m = (n - 4) // 2
+        print(((m + 1)*(m + 2)) // 2)
