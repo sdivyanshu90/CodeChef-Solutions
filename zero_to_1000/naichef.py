769a092f7a514b1626dab584c2886224efe0a794
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-04 11:10:35 +0530

    Naive Chef

---
+++ zero_to_1000/naichef.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, a, b = map(int, input().split())
+    x = list(map(int, input().split()))
+    counta, countb = 0, 0
+    for i in x:
+        if i == a:
+            counta += 1
+    for i in x:
+        if i == b:
+            countb += 1
+        
+    print((counta * countb) / (n**2))
