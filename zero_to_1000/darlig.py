a13006321ae6c4036bbc6a4d05efe87cdf42b823
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-06 17:20:40 +0530

    Dark Light

---
+++ zero_to_1000/darlig.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    if k == 0:
+        if n % 4 != 0:
+            print("On")
+        else:
+            print("Off")
+    else:
+        if n % 4 != 0:
+            print("Ambiguous")
+        else:
+            print("On")
