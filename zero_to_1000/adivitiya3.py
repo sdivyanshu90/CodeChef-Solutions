1c2dd21f7a72be848fbec98f118d10439eb7c2d8
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-06 17:36:02 +0530

    Cookie Day

---
+++ zero_to_1000/adivitiya3.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    res = []
+    for num in a:
+        if num >= k:
+            res.append(num % k)
+    if not res:
+        print(-1)
+    else:
+        print(min(res))
