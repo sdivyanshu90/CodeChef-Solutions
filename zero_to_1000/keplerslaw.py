606fd352f030be3d1370e90427df3587444c9416
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-06 17:19:26 +0530

    Keplers Law

---
+++ zero_to_1000/keplerslaw.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    t1, t2, r1, r2 = map(int, input().split())
+    first = t1 ** 2 / r1 ** 3
+    second = t2 ** 2 / r2 ** 3
+    if first == second:
+        print("Yes")
+    else:
+        print("No")
