837611d0edf08f9e642a350a56ddc8ed7c31564b
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-06 17:44:42 +0530

    Kitchen Timetable

---
+++ zero_to_1000/kttable.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    
+    res = 0
+    if b[0] <= a[0]:
+        res += 1
+    for i in range(1, n):
+        if b[i] <= a[i] - a[i-1]:
+            res += 1
+    
+    print(res)
