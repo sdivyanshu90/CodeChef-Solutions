fa44947fb2db236306978dc9df518ae9140afce7
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-06 17:16:24 +0530

    Moody Chef

---
+++ zero_to_1000/moochef.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, l, r = map(int, input().split())
+    a = list(map(int, input().split()))
+    maxi, mini, happiness = 0, 0, 0
+    for num in a:
+        if l <= num and num <= r:
+            happiness += 1
+        else:
+            happiness += -1
+        maxi = max(maxi, happiness)
+        mini = min(mini, happiness)
+    print(maxi, mini)
