5ebb70f6e7e01f3aa1ca9cebecc0784fe56b5c6d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-06 17:28:15 +0530

    Dice Number

---
+++ zero_to_1000/dicenum.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+    a1, a2, a3, b1, b2, b3 = map(int, input().split())
+    a = [a1, a2, a3]
+    b = [b1, b2, b3]
+    a.sort(reverse = True)
+    b.sort(reverse = True)
+    alice = int("".join(map(str, a)))
+    bob = int("".join(map(str, b)))
+    # print(alice, bob)
+    if alice == bob:
+        print("Tie")
+    elif alice > bob:
+        print("Alice")
+    else:
+        print("Bob")
