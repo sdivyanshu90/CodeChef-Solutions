4cd3b770344fc6b8b453d31c1b68846c9702ae59
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-09 13:26:15 +0530

    CodeChef Streak

---
+++ 1000_to_1200/cs2023_stk.py
@@ -0,0 +1,30 @@
+for _ in range(int(input())):
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    streak_count_a = 0
+    streak_count_b = 0
+    
+    current_streak_a = 0
+    for streak in A:
+        if streak > 0:
+            current_streak_a += 1
+            streak_count_a = max(streak_count_a, current_streak_a)
+        else:
+            current_streak_a = 0
+    
+    
+    current_streak_b = 0
+    for streak in B:
+        if streak > 0:
+            current_streak_b += 1
+            streak_count_b = max(streak_count_b, current_streak_b)
+        else:
+            current_streak_b = 0
+
+    if streak_count_a > streak_count_b:
+        print("Om")
+    elif streak_count_a < streak_count_b:
+        print("Addy")
+    else:
+        print("Draw")
