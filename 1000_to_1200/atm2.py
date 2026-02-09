88dc2d1de74440bdb67600635e164fd2a009c39c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-09 11:48:51 +0530

    ATM Machine

---
+++ 1000_to_1200/atm2.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    res = ""
+    for p in a:
+        if p > k:
+            res += "0"
+            continue
+        else:
+            k -= p
+            res += "1"
+    print(res)
