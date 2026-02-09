fe49330372c48e9594430e4cf15c58e4f698b2f9
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-09 11:39:03 +0530

    Easy Pronunciation

---
+++ 1000_to_1200/ezspeak.py
@@ -0,0 +1,18 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    consonant = 0
+    temp = 0
+    for char in s:
+        
+        if char not in vowel:
+            temp += 1
+        else:
+            temp = 0
+        consonant = max(temp, consonant)
+    if consonant >= 4:
+        print("NO")
+    else:
+        print("YES")
