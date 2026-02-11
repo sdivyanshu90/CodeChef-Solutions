57c22a91f490933416978c83eddd40613a5607a7
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-11 12:59:19 +0530

    Chef Diet

---
+++ 1000_to_1200/diet.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    stored = 0
+    fail = 0
+    for i in range(n):
+        stored += a[i]
+        if stored < k:
+            fail = i + 1
+            break
+        stored -= k
+    if fail:
+        print(f"NO {fail}")
+    else:
+        print("YES")
