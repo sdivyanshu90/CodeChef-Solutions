602bcc8c7afd97997d740ecb1f4202833ccfe68e
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-11 12:35:17 +0530

    Adjacent Sum Parity

---
+++ 1000_to_1200/adjsumpar.py
@@ -0,0 +1,15 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    b = list(map(int, input().split()))
+    zeros, ones = 0, 0
+    for bit in b:
+        if bit == 0:
+            zeros += 1
+        else:
+            ones += 1
+    
+    if ones % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
