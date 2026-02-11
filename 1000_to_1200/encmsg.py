ed7221aa56fccc94aa2d3a8a7dc805c72ba501bb
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-11 13:11:16 +0530

    Encoding Message

---
+++ 1000_to_1200/encmsg.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    chars = list(s)
+
+    for i in range(0, n - 1, 2):
+        chars[i], chars[i + 1] = chars[i + 1], chars[i]
+    
+    for i in range(n):
+        chars[i] = chr(ord('z') - (ord(chars[i]) - ord('a')))
+    
+    print(''.join(chars))
