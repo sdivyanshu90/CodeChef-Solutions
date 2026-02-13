69ed3b2ea24c623f7c839f1fd2e6b7fbd7caa136
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-13 12:24:28 +0530

    Buying New Tablet

---
+++ 1000_to_1200/tablet.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, budget = map(int, input().split())
+    temp = []
+    for _ in range(n):
+        w, h, p = map(int, input().split())
+        temp.append((w*h, p))
+        
+    res = 0
+    for area, price in temp:
+        if price <= budget:
+            res = max(res, area)
+    if res:
+        print(res)
+    else:
+        print("no tablet")
