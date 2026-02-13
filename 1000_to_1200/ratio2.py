11ecccc83df00daaa9768a83cfd23e2c77c894df
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-13 12:15:16 +0530

    Ratio By 2

---
+++ 1000_to_1200/ratio2.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x >= 2*y or y >= 2*x:
+        print(0)
+    else:
+        mini = min(x, y)
+        maxi = max(x, y)
+        print(abs((maxi // 2) - mini))
