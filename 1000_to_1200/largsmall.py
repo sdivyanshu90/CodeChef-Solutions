9560347e9648af0571a1938a100faecb570bae00
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-13 12:16:03 +0530

    Larger Smaller

---
+++ 1000_to_1200/largsmall.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    min_val = min(arr)
+    max_val = max(arr)
+    if max_val - min_val <= 1:
+        print(0)
+    else:
+        print(max_val - min_val - 1)
