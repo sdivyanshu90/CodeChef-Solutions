b3889bac7e3e24962dc5bab964c21c2653a2d2f1
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-13 12:05:05 +0530

    Program Your Own CALCULATOR

---
+++ 1000_to_1200/urcalc.py
@@ -0,0 +1,12 @@
+# cook your dish here
+a = int(input())
+b = int(input())
+s = input()
+if s == "+":
+    print(a + b)
+elif s == "-":
+    print(a - b)
+elif s == "*":
+    print(a * b)
+else:
+    print(a / b)
