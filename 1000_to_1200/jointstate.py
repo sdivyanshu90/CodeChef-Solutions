bf81152f2e842b143ca61a6a81bd57d49f50ab39
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-15 12:01:42 +0530

    Join States

---
+++ 1000_to_1200/jointstate.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    res = 0
+    cum_sum = 0
+    for num in a:
+        cum_sum += num
+        if cum_sum >= m:
+            res += 1
+            cum_sum = 0
+    print(res)
