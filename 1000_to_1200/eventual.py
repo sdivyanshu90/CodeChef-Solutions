c68456877e869e918004a56107ca4958fa861c90
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-15 11:51:04 +0530

    Even-tual Reduction

---
+++ 1000_to_1200/eventual.py
@@ -0,0 +1,25 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    freq = {}
+    for char in s:
+        if char not in freq:
+            freq[char] = 1
+        else:
+            freq[char] += 1
+            
+    # for char, frequency in freq.items():
+    #     print(f"Character: {char}, Frequency: {frequency}")
+    # print("--------------------END------------------------")
+    flag = True
+    for key, val in freq.items():
+        if val % 2 != 0:
+            flag = False
+            break
+            
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+            
