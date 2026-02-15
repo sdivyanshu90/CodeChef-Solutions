6d572b1cb4bed4dcd6f216661417a2a46e19fb74
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-15 12:36:40 +0530

    End Sorted

---
+++ 1000_to_1200/endsorted.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    pos1 = a.index(1)
+    posN = a.index(n)
+    
+    if pos1 < posN:
+        print(pos1 + (n - 1 - posN))
+    else:
+        print(pos1 + (n - 1 - posN) - 1)
