f84da6a4fc649d2bc56a78a25d09f8df15390464
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-15 11:53:46 +0530

    Chef and Subarrays

---
+++ 1000_to_1200/chefarrp.py
@@ -0,0 +1,20 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    count = 0
+    
+    for i in range(n):
+        s = 0
+        p = 1
+        for j in range(i, n):
+            s += a[j]
+            p *= a[j]
+            
+            if p == s:
+                count += 1
+            
+            if p > 10**15:
+                break
+                
+    print(count)
