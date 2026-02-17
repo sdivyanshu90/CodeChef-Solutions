7dc183ab155cb27df833740b5323cae329154d9a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-17 18:20:24 +0530

    Lost Weekends

---
+++ 1000_to_1200/lostwknd.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    a1, a2, a3, a4, a5, p = map(int, input().split())
+    tot = 0
+    a = [a1, a2, a3, a4, a5]
+    for num in a:
+        tot += (num * p)
+    hrs = 24*5
+    if tot <= hrs:
+        print("No")
+    else:
+        print("Yes")
