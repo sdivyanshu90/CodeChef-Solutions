83989134a86181ac758e8bdb564be917f75c62dd
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-17 17:47:23 +0530

    Chefland Library

---
+++ 1000_to_1200/borrowbook.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    occ = {}
+
+    for i, value in enumerate(a, start=1):
+        occ[value] = max(occ.get(value, 0), i)
+
+    print(sum(occ.values()))
