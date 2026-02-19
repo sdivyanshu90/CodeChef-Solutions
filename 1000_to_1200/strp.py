797792e3bbcaad79a14bcfb03d685414020ccd9d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-19 16:58:21 +0530

    String protocol

---
+++ 1000_to_1200/strp.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    count = 0
+    while count < len(s)-1:
+        if s[count] == s[count+1]:
+            count += 2
+            n -= 1
+        else:
+            count += 1
+    print(n)
