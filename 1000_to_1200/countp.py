7626bae9de55f3c67dafd098f20783dab9680d5b
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-19 16:59:21 +0530

    Counting Problem

---
+++ 1000_to_1200/countp.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    array = list(map(int, input().split()))
+    count = sum(1 for i in array if i % 2 == 1)
+    if count % 2 == 1 or count == 0:
+        print("NO")
+    else:
+        print("YES")
