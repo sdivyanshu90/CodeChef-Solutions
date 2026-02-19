0482abc784afdfe3ae5497f528696f8033c0bf3d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-19 16:57:36 +0530

    Tanu and Head-bob

---
+++ 1000_to_1200/headbob.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    int(input())
+    s = input().strip()
+    
+    if 'I' in s:
+        print("INDIAN")
+    elif 'Y' in s:
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")
