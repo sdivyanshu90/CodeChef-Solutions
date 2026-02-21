e24f7f846129d8770a7e957901cb90f395a997d5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-21 12:39:52 +0530

    Chef and Table Tennis

---
+++ 1000_to_1200/ttenis.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+    s = input().strip()
+    chef = 0
+    opponent = 0
+    for ch in s:
+        if ch == '1':
+            chef += 1
+        else:
+            opponent += 1
+        if (chef >= 11 or opponent >= 11) and abs(chef - opponent) >= 2:
+            break
+    if chef > opponent:
+        print("WIN")
+    else:
+        print("LOSE")
