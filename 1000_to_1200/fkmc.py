68b5a1e769aed85bb2b2f90bcd6484f2f7c57d1e
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-21 11:48:00 +0530

    Fake Certificate

---
+++ 1000_to_1200/fkmc.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    zero = 0
+    maxi = 0
+    ones = s.count("1")
+    for num in s:
+        if num == "0":
+            zero += 1
+        else:
+            zero = 0
+        maxi = max(zero, maxi)
+    print(maxi + ones)
