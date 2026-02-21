073d0ed55742931823e4edae9ef07a5376b46731
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-21 11:49:40 +0530

    The Attack of Queen

---
+++ 1000_to_1200/queenattack.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x, y = map(int, input().split())
+    row = 2 * (n - 1)
+    diagonal = (
+        min(x - 1, y - 1) + 
+        min(x - 1, n - y) + 
+        min(n - x, y - 1) + 
+        min(n - x, n - y)
+    )
+    print(row + diagonal)
