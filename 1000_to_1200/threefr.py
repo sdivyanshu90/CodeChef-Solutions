6f4e09b1b9ef0f5773b45c892d17e5fdc4432e14
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-21 11:47:12 +0530

    Three Friends

---
+++ 1000_to_1200/threefr.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    X, Y, Z = map(int, input().split())
+    
+    if X + Y == Z or Y + Z == X or Z + X == Y:
+        print("YES")
+    else:
+        print("NO")
