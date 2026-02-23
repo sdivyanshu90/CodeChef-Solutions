c2212a3256e6e7f01053ec8b505699bfd4de2c1a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-23 12:50:56 +0530

    Holes in the text

---
+++ 1000_to_1200/holes.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    s = input()
+    res = 0
+    for char in s:
+        if char in ["A", "D", "O", "P", "Q", "R"]:
+            res += 1
+        elif char == "B":
+            res += 2
+    print(res)
