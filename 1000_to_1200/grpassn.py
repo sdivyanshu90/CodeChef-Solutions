36087dce02ab37ff692eacaa2716946531a4fd63
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-23 12:50:20 +0530

    Group Assignment

---
+++ 1000_to_1200/grpassn.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    p = list(map(int, input().split()))
+    peoples = set(p)
+    for i in peoples:
+        if p.count(i) % i != 0:
+            print("NO")
+            break
+    else:
+        print("YES")
