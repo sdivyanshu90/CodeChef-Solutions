cdc32a2db632e3ab9c91f82938ee1302e7d287d8
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-23 12:47:59 +0530

    Magician versus Chef

---
+++ 1000_to_1200/magichf.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x, s = map(int, input().split())
+    res = x
+    for _ in range(s):
+        a, b = map(int, input().split())
+        if res == a:
+            res = b
+        elif res == b:
+            res = a
+    print(res)
