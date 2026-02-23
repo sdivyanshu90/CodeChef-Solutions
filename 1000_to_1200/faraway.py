89e109a890eb82e1857c0b407d173da3cc5aec1c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-23 12:48:49 +0530

    Far Away

---
+++ 1000_to_1200/faraway.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for _ in range(int(input())):
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    summ , maxi = 0, 0
+    for i in range(N):
+        if A[i] > M // 2:
+            maxi = abs(A[i] - 1)
+        else:
+            maxi = abs(A[i] - M)
+        
+        summ = summ + maxi
+        maxi = 0
+    print(summ)
