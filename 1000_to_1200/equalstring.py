158a7842c22d1b61d87506652f601a6451219f2f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-23 12:49:36 +0530

    Equal Strings

---
+++ 1000_to_1200/equalstring.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = input()
+    b = input()
+    unique = set()
+    for i in range(n):
+        if a[i] != b[i]:
+            unique.add(b[i])
+    print(len(unique))
