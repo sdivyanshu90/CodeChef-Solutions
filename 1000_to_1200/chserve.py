3b0cf85d4c301c7882854133e50586da23b3f31b
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-25 14:54:49 +0530

    Chef and Serves

---
+++ 1000_to_1200/chserve.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    chef, cook, k = map(int, input().split())
+    blocks = (chef + cook) // k
+    if blocks % 2 == 0:
+        print("CHEF")
+    else:
+        print("COOK")
