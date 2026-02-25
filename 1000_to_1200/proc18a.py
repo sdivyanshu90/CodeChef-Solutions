aae33339f3713b24cf7617c7afde2c8ea3ef3a79
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-25 13:49:17 +0530

    The Great Run

---
+++ 1000_to_1200/proc18a.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    res = []
+    for i in range(n - k + 1):
+        window = a[i : i + k]
+        res.append(sum(window))
+        
+    print(max(res))
