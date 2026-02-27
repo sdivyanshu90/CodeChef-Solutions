8026ba736b519e34d33e241af07371c567b089c9
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-02-27 12:54:13 +0530

    Dice Deception

---
+++ 1000_to_1200/dide.py
@@ -0,0 +1,20 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    mapped = {
+        1: 6,
+        2: 5,
+        3: 4
+    }
+    
+    res = sum(a)
+    gains = []
+    
+    for num in a:
+        if num <= 3:
+            gains.append(mapped[num] - num)
+    gains.sort(reverse=True)
+    res += sum(gains[:k])
+    print(res)
