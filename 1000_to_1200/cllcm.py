3712887799d136d59ed52a93d43423563ea5f3a4
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-01 12:25:03 +0530

    Chef vs Doof

---
+++ 1000_to_1200/cllcm.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    if any(i % 2 == 0 for i in a):
+        print("NO")
+    else:
+        print("YES")
