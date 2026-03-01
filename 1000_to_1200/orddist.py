4f26189851101e556294d34d374395462f1ca53d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-01 12:58:00 +0530

    Ordered Distances

---
+++ 1000_to_1200/orddist.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    x = list(map(int, input().split()))
+    y = list(map(int, input().split()))
+    
+    p = y[0]
+    order = sorted(range(n), key=lambda i: (abs(x[i] - p), x[i]))
+    
+    if all(y[i] == x[order[i]] for i in range(n)):
+        print(order[0] + 1)
+    else:
+        print(-1)
