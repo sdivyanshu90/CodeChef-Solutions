5179888163bd7a9043186e26a61028f8eeb945d1
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-01 12:22:54 +0530

    Playing with Strings

---
+++ 1000_to_1200/playstr.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    r = input()
+    zero_s, one_s, zero_r, one_r = s.count("0"), s.count("1"), r.count("0"), r.count("1")
+    
+    if zero_r == zero_s and one_s == one_r:
+        print("YES")
+    else:
+        print("NO")
