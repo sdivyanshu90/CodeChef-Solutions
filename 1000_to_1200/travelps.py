c53eb6540df02dd6d9a1659c57098dfe993bd6d7
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-03 12:56:11 +0530

    Travel Pass

---
+++ 1000_to_1200/travelps.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, a, b = map(int, input().split())
+    s = input()
+    zeros, ones = 0, 0
+    for num in s:
+        if num == "0":
+            zeros += 1
+        else:
+            ones += 1
+            
+    print(zeros * a + ones * b)
