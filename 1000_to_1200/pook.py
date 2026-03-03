cd24346373594a38c79a37d833dcf63bd9d22928
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-03 15:14:11 +0530

    Game of Pooks

---
+++ 1000_to_1200/pook.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    if n > 3:
+        print(n)
+    else:
+        if n > 1:
+            print(n - 1)
+        else:
+            print(1)
