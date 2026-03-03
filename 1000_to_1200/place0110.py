4cede21daa1cf4ebc344ff84e2a5ef172c57e4f5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-03 12:41:48 +0530

    Placing 01 And 10

---
+++ 1000_to_1200/place0110.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if abs(x - y) <= 1:
+        print(x + y)
+    else:
+        print(2 * max(x, y) - 1)
