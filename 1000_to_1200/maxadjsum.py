b7bc5ba7feff6ecf8cc0b8a660d21a2d7d3ff6f5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-03 12:57:29 +0530

    Maximise Adjacent Sum

---
+++ 1000_to_1200/maxadjsum.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    
+    total = sum(a)
+    answer = 2 * total - a[0] - a[1]
+    print(answer)
