78e415bcea76b34bfad33b252ac6a57856beeeb3
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-03 12:55:21 +0530

    FlatLand

---
+++ 1000_to_1200/icl1902.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    res = 0
+    
+    while n != 0:
+        s = int((n) ** 0.5)
+        n -= (s * s)
+        res += 1
+    
+    print(res)
