8f37ee4d42e12efbe2d477a6b2489dc8d1cf0230
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-03 12:54:34 +0530

    Alternating String

---
+++ 1000_to_1200/altstr.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    
+    c0 = s.count("0")
+    c1 = s.count("1")
+    
+    print(min(n, 2 * min(c0, c1) + 1))
