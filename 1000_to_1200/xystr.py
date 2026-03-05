fb4be71691d2914c0e4ded70d2f7ffaaae47033a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-05 11:46:54 +0530

    Chef and String

---
+++ 1000_to_1200/xystr.py
@@ -0,0 +1,14 @@
+# cook your dish here
+for _ in range(int(input())):
+    s = input().strip()
+    i = 0
+    pairs = 0
+    
+    while i < len(s) - 1:
+        if s[i] != s[i+1]:
+            pairs += 1
+            i += 2
+        else:
+            i += 1
+            
+    print(pairs)
