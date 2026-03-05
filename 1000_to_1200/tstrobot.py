42508c3ebe3d40f5c1c1d0f805e8ddcd1118717a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-05 11:41:41 +0530

    Testing Robot

---
+++ 1000_to_1200/tstrobot.py
@@ -0,0 +1,17 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    s = input()
+    
+    moves = set()
+    moves.add(x)
+    for step in s:
+        if step == "R":
+            x += 1
+            
+        else:
+            x -= 1
+        moves.add(x)
+            
+    print(len(moves))
+    # print(moves)
