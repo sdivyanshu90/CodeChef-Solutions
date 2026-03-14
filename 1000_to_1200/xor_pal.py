11bde7e0e00d26da1c7da98f406e15ccee325eab
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-14 12:15:54 +0530

    Xor Palindrome

---
+++ 1000_to_1200/xor_pal.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    target = s[::-1]
+    
+    idx = []
+    for i in range(len(s)):
+        if s[i] != target[i]:
+            idx.append(i + 1)
+            
+    print(idx)
