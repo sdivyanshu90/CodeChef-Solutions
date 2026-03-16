0bec8566348c38373c4052f59d27601402b5f83b
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-16 11:50:31 +0530

    Multiple Choice Exam

---
+++ 1000_to_1200/exam1.py
@@ -0,0 +1,21 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    u = input()
+    
+    score = 0
+    skip = False
+    
+    for i in range(n):
+        if skip:
+            skip = False
+            continue
+        
+        if u[i] == s[i]:
+            score += 1
+        else:
+            if u[i] != 'N':
+                skip = True
+    
+    print(score)
