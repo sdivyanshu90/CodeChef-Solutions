02b1361c10af2cd0850454749777f6e373843725
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-16 11:59:55 +0530

    Game of Piles Version 1

---
+++ 1000_to_1200/gameofpiles1.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    if 1 in a:
+        print('CHEF')
+    elif a.count(2) == n:
+        print('CHEFINA')
+    else:
+        score = sum(i - 2 for i in a if i > 2)
+        print('CHEFINA' if score % 2 == 0 else 'CHEF')
