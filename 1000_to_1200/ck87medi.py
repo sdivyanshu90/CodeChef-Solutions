d834d120ab22314c0d16fe631f0b0eca1939d190
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-18 13:28:34 +0530

    Chef and Employment Test

---
+++ 1000_to_1200/ck87medi.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    arr = sorted(map(int, input().split()))
+    
+    arr.extend([max(arr) + 1] * k)
+    print(arr[len(arr) // 2])
