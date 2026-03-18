041592edc77ba6ea486757abd7d17db4247f4c5d
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-18 13:35:25 +0530

    Max Binary

---
+++ 1000_to_1200/max_bin.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    s = list(input())
+    
+    if s[0] != "1":
+        s[0] = "1"
+        k -= 1
+    s.append("0" * k)
+    print("".join(s))
