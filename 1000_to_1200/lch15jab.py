125d6eed6f4483e2a58be89b42cca82f1190c6fb
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-18 13:37:25 +0530

    Piece of cake

---
+++ 1000_to_1200/lch15jab.py
@@ -0,0 +1,16 @@
+# cook your dish here
+from collections import Counter
+
+for _ in range(int(input())):    
+    input_string = input()
+    
+    char_count = Counter(input_string)
+    
+    most_common_char, most_common_count = char_count.most_common(1)[0]
+    
+    sum_of_other_counts = sum(count for char, count in char_count.items() if char != most_common_char)
+    
+    if most_common_count == sum_of_other_counts:
+        print("YES")
+    else:
+        print("NO")
