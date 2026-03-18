f0d7512f1ae2189170fbcee7b0875e0e2910ab7f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-18 13:32:05 +0530

    Priya and AND

---
+++ 1000_to_1200/cens20d.py
@@ -0,0 +1,17 @@
+# cook your dish here
+from collections import defaultdict
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    count = 0
+    bit_count = defaultdict(int)
+
+    for num in arr:
+        for key in list(bit_count):
+            if num & key == key:
+                count += bit_count[key]
+        bit_count[num] += 1
+    
+    print(count)
