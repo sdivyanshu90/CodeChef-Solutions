c225bf1a89901636fd83b268771fca2dcf7f7c28
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-22 15:49:06 +0530

    Bear and Segment 01

---
+++ 1000_to_1200/segm01.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    s = input()
+    if '1' not in s:
+        print('NO')
+    else:
+        start = s.index('1')
+        end = s.rindex('1')
+        if '0' in s[start:end+1]:
+            print('NO')
+        else:
+            print('YES')
