d77318887021dbeeea4166b5884c6804a8b027f5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-03-22 13:51:59 +0530

    Final Sum

---
+++ 1000_to_1200/finalsum.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, q = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    res = sum(a)
+    for _ in range(q):
+        l, r = map(int, input().split())
+        d = r - l + 1
+        if d % 2 != 0:
+            res += 1
+    print(res)
