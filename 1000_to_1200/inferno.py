68733110867d2662d376254c90a81e14ffa7a01f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-01 12:59:18 +0530

    Infernos

---
+++ 1000_to_1200/inferno.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    h = list(map(int, input().split()))
+
+    m1 = max(h)
+    m2 = sum((i + x - 1) // x for i in h)
+    print(min(m1, m2))
