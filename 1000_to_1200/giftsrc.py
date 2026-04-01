967ec63e067e20dd9419761fb5dc8ad06f3ac7ae
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-01 12:32:29 +0530

    Find Your Gift

---
+++ 1000_to_1200/giftsrc.py
@@ -0,0 +1,29 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input().strip()
+    
+    x, y = 0, 0
+    prev_axis = None
+    
+    for direction in s:
+        if direction in "LR":
+            axis = 'X'
+        else:
+            axis = 'Y'
+        
+        if axis == prev_axis:
+            continue
+        
+        if direction == "L":
+            x -= 1
+        elif direction == "R":
+            x += 1
+        elif direction == "U":
+            y += 1
+        else:
+            y -= 1
+        
+        prev_axis = axis
+    
+    print(x, y)
