7512d7e8b46dbf9c209c8bdb483ff8e58b09acdf
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-01 12:33:11 +0530

    Diagonal movement

---
+++ 1000_to_1200/diagmove.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    diff = abs(x - y)
+    if diff % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
