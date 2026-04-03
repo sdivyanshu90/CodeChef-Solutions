9012c4a3d0554aab6342c093a9423d34e0c596c6
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-03 13:30:15 +0530

    Hostel Room

---
+++ 1000_to_1200/hostelroom.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    arr = list(map(int, input().split()))
+    
+    res = x
+    for num in arr:
+        x += num
+        res = max(res, x)
+    print(res)
