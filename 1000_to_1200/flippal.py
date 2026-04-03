2692ba07cd67ea40cd262fc5cffc510220ed3714
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-03 13:42:42 +0530

    Palindrome Flipping

---
+++ 1000_to_1200/flippal.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    
+    count_1 = s.count('1')
+    count_0 = n - count_1
+
+    if count_1 % 2 != 0 and count_0 % 2 != 0:
+        print("NO")
+    else:
+        print("YES")
