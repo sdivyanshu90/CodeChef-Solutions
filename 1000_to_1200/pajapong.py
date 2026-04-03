1097beef944cf7b0d08094d461da38b4c47e2fca
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-03 13:40:57 +0530

    Ping Paja Pong

---
+++ 1000_to_1200/pajapong.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    x, y, k = map(int, input().split())
+    
+    if ((x + y) // k) % 2 == 0:
+        print('Chef')
+    else:
+        print('Paja')
