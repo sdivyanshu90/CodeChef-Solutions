ca3f816e1f65b71cbd706f01c0f441d8e8041387
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-03 13:37:50 +0530

    Dominant Element

---
+++ 1000_to_1200/dominant2.py
@@ -0,0 +1,29 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    freq = {}
+    
+    for num in arr:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+            
+    count = []
+    for key, val in freq.items():
+        count.append(val)
+        
+    # print(count)
+    
+    maxi = max(count)
+    count.remove(maxi)
+    same = False
+    for c in count:
+        if c == maxi:
+            same = True
+            
+    if same:
+        print("NO")
+    else:
+        print("YES")
