1f1ad399f905f894548f376c664cc23372d009da
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-05 15:27:00 +0530

    Sale

---
+++ 1000_to_1200/finalsale.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    max_total = 0
+    
+    for i in range(n):
+        sums = sum(arr[:i])
+        total = sums + (2 * arr[i])
+        max_total = max(max_total, total)
+    
+    print(max_total)
