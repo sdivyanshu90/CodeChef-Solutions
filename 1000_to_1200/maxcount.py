e2d118c5c8ef46d595b32a059c5e13420f8e379a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-05 15:14:43 +0530

    Count of Maximum

---
+++ 1000_to_1200/maxcount.py
@@ -0,0 +1,20 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    freq = {}
+    for num in arr:
+        freq[num] = freq.get(num, 0) + 1
+    
+    max_freq = 0
+    answer = float('inf')
+    
+    for num in freq:
+        if freq[num] > max_freq:
+            max_freq = freq[num]
+            answer = num
+        elif freq[num] == max_freq:
+            answer = min(answer, num)
+    
+    print(answer, max_freq)
