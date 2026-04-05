27926b333edf5f94cb8dc22b4464d6c9eb4bb8b7
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-05 14:36:03 +0530

    A - Books

---
+++ 1000_to_1200/bit2a.py
@@ -0,0 +1,15 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    res = [0] * n
+    
+    for i in range(n):
+        count = 0
+        for j in range(i + 1, n):
+            if arr[j] > arr[i]:
+                count += 1
+                
+        res[i] = count
+        
+    print(*res)
