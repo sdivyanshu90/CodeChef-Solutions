53b1b748c841a33572c8c65bf2bee4926c7c4955
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-07 15:24:37 +0530

    Divisible by i

---
+++ 1000_to_1200/divbyi.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    i, j = 0, n - 1
+    res = []
+    
+    while i < j:
+        res.append(j + 1)
+        res.append(i + 1)
+        i += 1
+        j -= 1
+    
+    if i == j:
+        res.append(i + 1)
+    
+    print(*res[::-1])
