29945058a304e130a85dba621e7edfb3df3b8632
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-07 15:22:37 +0530

    Three Boxes

---
+++ 1000_to_1200/threebox.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    if a + b + c <= d:
+        print(1)
+    elif a + b <= d or b + c  <= d or a + c <= d:
+        print(2)
+    else:
+        print(3)
