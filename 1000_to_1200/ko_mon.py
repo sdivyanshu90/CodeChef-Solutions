630e9d251bb4d9c8adba76034f7696446b369151
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-07 15:25:21 +0530

    Monster Monster

---
+++ 1000_to_1200/ko_mon.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, x = map(int, input().split())
+    monster_strengths = list(map(int, input().split()))
+    
+    monster_strengths.sort(reverse=True)
+    power = monster_strengths[0]
+    for idx in range(1, n):
+        power = max(power, monster_strengths[idx] + idx * x)
+    print(power)
