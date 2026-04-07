68740ff1a93cd21da0fe226d146b2308606159e4
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-07 15:23:25 +0530

    A Balanced Contest

---
+++ 1000_to_1200/perfcont.py
@@ -0,0 +1,20 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, p = map(int, input().split())
+    arr = list(map(int, input().split()))
+        
+    easy, hard = 0, 0
+    half = p // 2
+    tenth = p // 10
+    for num in arr:
+        if num >= half:
+            easy += 1
+        elif tenth >= num:
+            hard += 1
+            
+    # print(easy, hard)
+            
+    if easy == 1 and hard == 2:
+        print("yes")
+    else:
+        print("no")
