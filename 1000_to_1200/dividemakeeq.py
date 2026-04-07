38c7bdd41994014e792c627f90d8f1e1608a0909
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-07 16:01:18 +0530

    Luigi and Uniformity

---
+++ 1000_to_1200/dividemakeeq.py
@@ -0,0 +1,13 @@
+# cook your dish here
+from math import gcd
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    divisor = gcd(*a)
+    count = 0
+    for num in a:
+        if num > divisor:
+            count += 1
+    print(count)
