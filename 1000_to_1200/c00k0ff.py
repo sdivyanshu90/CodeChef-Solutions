b553e979d27a2d0787fd67dc39a5ccf934586cc0
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-07 15:59:51 +0530

    Chef and Cook-Off Contests

---
+++ 1000_to_1200/c00k0ff.py
@@ -0,0 +1,21 @@
+# cook your dish here
+difficulty_map = {
+    'cakewalk': 1,
+    'easy': 2,
+    'simple': 3,
+    'easy-medium': 4,
+    'medium': 4,
+    'medium-hard': 5,
+    'hard': 5
+}
+
+for _ in range(int(input())):
+    difficulty_set = set()
+    for _ in range(int(input())):
+        s = input().strip()
+        difficulty_set.add(difficulty_map.get(s, 0))
+    
+    if difficulty_set == {1, 2, 3, 4, 5}:
+        print('Yes')
+    else:
+        print('No')
