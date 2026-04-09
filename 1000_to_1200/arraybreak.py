07b30b77acd084539e387098e0f31cf8813fce79
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-09 16:18:41 +0530

    Break the elements

---
+++ 1000_to_1200/arraybreak.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    count_even = sum(1 for i in a if i % 2 == 0)
+    print(0 if count_even == n else count_even)
