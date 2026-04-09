453c76e2452218608077bea372101284aebecf7a
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-09 16:16:38 +0530

    Swish Game

---
+++ 1000_to_1200/swishgame.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    m, k = map(int, input().split())
+    s = input()
+    
+    count_S = s.count('S')
+    
+    if count_S >= k:
+        print(m)
+    else:
+        print(m + (k - count_S - 1))
