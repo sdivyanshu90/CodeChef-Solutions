e1ba1216174300aa4292073c749667f5df9e13e5
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-09 16:47:12 +0530

    OR Permutation

---
+++ 1000_to_1200/permor.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for _ in range(int(input())):
+    N = int(input())
+    permutation = list(range(1, N + 1))
+    for i in range(2, N, 2):
+        permutation[i], permutation[i - 1] = permutation[i - 1], permutation[i]
+    print(" ".join(map(str, permutation)))
