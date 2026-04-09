c512fec3582fbc092b690aaf000fc7688134bfe8
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-09 16:41:14 +0530

    Retrieve the Array

---
+++ 1000_to_1200/arrayret.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    b = list(map(int, input().split()))
+    
+    s = sum(b)
+    divisor = s // (n + 1)
+    l = [x - divisor for x in b]
+    print(*l)
