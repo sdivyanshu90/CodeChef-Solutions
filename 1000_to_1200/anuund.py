f99e80786b3d06ea422bfbeab153576db44f447c
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-11 16:03:30 +0530

    Ups and Downs

---
+++ 1000_to_1200/anuund.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = sorted(map(int, input().split()))
+    
+    if n > 2:
+        for i in range(1, n - 1, 2):
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    
+    print(*arr)
