f12e62e414dadc82bdb3b29cd18d6b2cfd2c7a9f
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-11 16:05:35 +0530

    Trace of Matrix

---
+++ 1000_to_1200/trace.py
@@ -0,0 +1,15 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    m = [list(map(int, input().split())) for _ in range(n)]
+    max_sum = 0
+
+    for i in range(n):
+        diag_sum = sum(m[j + i][j] for j in range(n - i))
+        max_sum = max(max_sum, diag_sum)
+
+    for j in range(1, n):
+        diag_sum = sum(m[i][i + j] for i in range(n - j))
+        max_sum = max(max_sum, diag_sum)
+
+    print(max_sum)
