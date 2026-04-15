6b298d4ec758de34285dc8216d466380fc25b570
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-15 14:22:46 +0530

    Wolf Down

---
+++ 1000_to_1200/wolfdown.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    idx = s.find("1")
+    # print(idx)
+    if idx == -1:
+        print(len(s))
+    else:
+        print(idx)
