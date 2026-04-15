d6e50f0129e844cb4db3c4a4e903334246c9ae45
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-15 14:25:04 +0530

    No Odd Sum

---
+++ 1000_to_1200/noddsm.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    odd = 0
+    for x in arr:
+        if x % 2 == 1:
+            odd += 1
+
+    even = n - odd
+
+    if odd % 2 == 0:
+        print(min(even, odd // 2))
+    else:
+        print(even)
