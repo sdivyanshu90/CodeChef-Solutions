6ef0af573df2a7ef43ee5d9d36d515f8a6339e64
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-04-15 14:21:52 +0530

    Add 1 or 3

---
+++ 1000_to_1200/add13.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    diff = m - n
+    if diff % 2 != 0 or m < n or m > 3*n:
+        print("NO")
+    else:
+        print("YES")
